a5e5e0d58d014f4965b6b0ab1023188fef9a256d
David H <davidhorph@hotmail.com>
2018-07-05 01:30:55 -0500

    animation-play-state, paused
    added an animation play state.

--- animations.js
+++ animations.js
@@ -2,6 +2,7 @@
 
 $("h1").click(function() {
     $(this).text("Aaaarrrggghhh!!! You got me!");
-    $(this).removeClass("startFly");    
+    // $(this).removeClass("startFly");    
+    $(this).css("animation-play-state","paused");
 });
 
