221bc36d2e36b31ac7730ef0e89a4144f937f103
David H <davidhorph@hotmail.com>
2018-07-05 19:42:57 -0500

    Will changing the i to properties...
    inside the J-Query work?

--- animations.js
+++ animations.js
@@ -1,9 +1,9 @@
 
 
-$("i").click(function() {
-    $(this).css("color", "red");      
+$("#properties").click(function() {
+    $(this).css("color","red");      
     $(this).css("animation-play-state","paused");    
-    $(this).css("color", "red") ? $("#msg").text("SQUASHED!!!") : $("span").text();
+    $(this).css("color","red") ? $("#msg").text("SQUASHED!!!") : $("span").text();
 });
 
 
