1b2e5bba75cc1d0e2fdec7b22269c7cb27c120f3
David H <davidhorph@hotmail.com>
2018-07-05 20:08:37 -0500

    Fixed it!!!
    Property was set to 100% height and width which accuonted for the inaccuracy.

--- animations.js
+++ animations.js
@@ -1,6 +1,6 @@
 
 
-$("#properties").click(function() {
+$(".properties").click(function() {
     $(this).css("color","red");      
     $(this).css("animation-play-state","paused");    
     $(this).css("color","red") ? $("#msg").text("SQUASHED!!!") : $("span").text();
